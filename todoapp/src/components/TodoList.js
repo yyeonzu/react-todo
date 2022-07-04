8aa9f2c4e08b3ff68a67f6460279f47d999aa5ab
yyeonzu <tooma22@naver.com>
2022-07-05 02:20:46 +0900

    feat: todos 배열 렌더링
    todos 배열을 map을 이용해 여러개의 todoItem 컴포넌트를 렌더링함.

--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
 import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
@@ -10,10 +11,17 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+	const todos = useTodoState();
 	return (
 		<TodoListBlock>
-			<TodoItem text="test텍스트" done={true} />
-			<TodoItem text="해야할 일" done={false} />
+			{todos.map(todo => (
+				<TodoItem
+					key={todo.id}
+					id={todo.id}
+					text={todo.text}
+					done={todo.done}
+				/>
+			))}
 		</TodoListBlock>
 	);
 }
